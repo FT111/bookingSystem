28d0aefda2c36b53d3334abdf6b92cdc509fe44b
FT111 <thetaylor2016@gmail.com>
2024-04-16 23:43:59 +0100

    Email details fetched from env variables

--- routes/sharedInstances.py
+++ routes/sharedInstances.py
@@ -1,6 +1,11 @@
+import os
 import bookingSystem as bSystem
 
 
-bs = bSystem.BookingSystem(dbPath='./instance/bookingDatabase.db')
+bs = bSystem.BookingSystem(dbPath='./instance/bookingDatabase.db',
+                           emailAddress=os.environ['EMAIL_ADDRESS'],
+                           emailAuth=os.environ['EMAIL_AUTH'],
+                           emailProvider=os.environ['EMAIL_PROVIDER'],
+                           emailPort=int(os.environ['EMAIL_PORT']))
 
 
