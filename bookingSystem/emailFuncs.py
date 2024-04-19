fd64f02604fcea099e050b99791424cb3745263a
FT111 <thetaylor2016@gmail.com>
2024-04-19 15:51:10 +0100

    Ticket QRs now sent with confirmation emails.

--- bookingSystem/emailFuncs.py
+++ bookingSystem/emailFuncs.py
@@ -1,5 +1,6 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 import smtplib
 from flask import render_template
 
@@ -11,12 +12,19 @@ class EmailFuncs:
         self.emailProvider = emailProvider
         self.emailPort = emailPort
 
-    def sendHTMLMail(self, toAddress, subject, body):
+    def sendHTMLMail(self, toAddress, subject, body, imageLocs: list[str] = None):
         msg = MIMEMultipart()
         msg['From'] = self.emailAddress
         msg['To'] = toAddress
         msg['Subject'] = subject
         msg.attach(MIMEText(body, 'html'))
+
+        if imageLocs is not None:
+            for imageLoc in imageLocs:
+                with open(imageLoc, 'rb') as imageFile:
+                    msgImage = MIMEImage(imageFile.read())
+                    msgImage.add_header('Content-ID', f'<{imageLoc}>')
+                    msg.attach(msgImage)
 
         try:
             with smtplib.SMTP(self.emailProvider, self.emailPort) as server:
@@ -35,8 +43,10 @@ class EmailFuncs:
         priceSum = bookingObj.getPriceSum()
         dateFormatted = viewing.getFormattedDate()
 
+        qrLocs = [f'./static/assets/codes/{ticket.getID()}.png' for ticket in tickets]
+
         # Renders the email body from Jinja template
         emailBody = render_template('./emailFormats/bookingConfirmed.html', booking=booking, dateFormatted=dateFormatted,
                                     seats=seats, tickets=tickets, viewing=viewing, customer=customer, priceSum=priceSum)
 
-        self.sendHTMLMail(toAddress, 'Booking Confirmation', emailBody)
+        self.sendHTMLMail(toAddress, 'Booking Confirmation', emailBody, qrLocs)
